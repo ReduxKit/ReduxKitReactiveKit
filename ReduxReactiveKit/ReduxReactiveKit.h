d6a9c407462d9bf6502f7a0d38bf73d11e7ae039
Karl Bowden <karl@karlbowden.com>
2015-12-20 10:54:13 +1100

    Scaffold taget platforms - iOS, OSX, tvOS, watchOS

--- ReduxReactiveKit/ReduxReactiveKit.h
+++ ReduxReactiveKit/ReduxReactiveKit.h
@@ -3,17 +3,13 @@
 //  ReduxReactiveKit
 //
 //  Created by Karl Bowden on 20/12/2015.
-//  Copyright © 2015 Redux. All rights reserved.
+//  Copyright © 2015 SwiftRedux. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for ReduxReactiveKit.
 FOUNDATION_EXPORT double ReduxReactiveKitVersionNumber;
 
 //! Project version string for ReduxReactiveKit.
 FOUNDATION_EXPORT const unsigned char ReduxReactiveKitVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ReduxReactiveKit/PublicHeader.h>
-
-
