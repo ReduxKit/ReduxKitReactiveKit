ce1ef4bcaec1fc984c43a8783710e294055555bc
Karl Bowden <karl@karlbowden.com>
2015-12-20 10:41:59 +1100

    Initial Commit

---
+++ ReduxReactiveKit/ReduxReactiveKit.h
@@ -0,0 +1,19 @@
+//
+//  ReduxReactiveKit.h
+//  ReduxReactiveKit
+//
+//  Created by Karl Bowden on 20/12/2015.
+//  Copyright © 2015 Redux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReduxReactiveKit.
+FOUNDATION_EXPORT double ReduxReactiveKitVersionNumber;
+
+//! Project version string for ReduxReactiveKit.
+FOUNDATION_EXPORT const unsigned char ReduxReactiveKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReduxReactiveKit/PublicHeader.h>
+
+
